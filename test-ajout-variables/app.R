45806ba4cc298584deb9c9f746ac6c9a6a40aa99
fxjollois <fxjolloisupd@gmail.com>
2017-08-10 10:34:20 +0200

    test d'ajout de variables - affichage table et barplot

--- test-ajout-variables/app.R
+++ test-ajout-variables/app.R
@@ -40,30 +40,34 @@ shinyApp(
                 insertUI(
                     paste0("#newVars"),
                     "beforeEnd",
-                    fluidRow(
-                        id = paste0("newVars", e),
-                        column(width = 3, textInput(paste0("newVars", e, ".nom"), "Nom de la variable", paste0("nouvelle", e), placeholder = "donner le nom de la nouvelle variable")),
-                        column(width = 3, selectInput(paste0("newVars", e, ".var"), "Variable de départ", names(donnees()))),
-                        column(width = 3, textInput(paste0("newVars", e, ".cut"), "Découpage", 5, placeholder = "indiquer ici les bornes des intervalles"))
-                        #,
-                        #column(width = 3, plotOutput(paste0("newVars", e, ".bar")))
+                    list(
+                        fluidRow(
+                            id = paste0("newVars", e),
+                            column(width = 3, textInput(paste0("newVars", e, ".nom"), "Nom de la variable", paste0("nouvelle", e), placeholder = "donner le nom de la nouvelle variable")),
+                            column(width = 3, selectInput(paste0("newVars", e, ".var"), "Variable de départ", names(donnees()))),
+                            column(width = 6, textInput(paste0("newVars", e, ".cut"), "Découpage", 5, placeholder = "indiquer ici les bornes des intervalles"))
+                        ),
+                        tableOutput(paste0("newVars", e, ".tab")),
+                        plotOutput(paste0("newVars", e, ".bar"))
                     )
                 )
                 
-                # input[[paste0("newVars", e, ".bar")]] <- renderPlot({
-                #     v = donnees()[,input[[paste0("newVars", e, ".var")]],]
-                #     c = as.numeric(strsplit(input[[paste0("newVars", e, ".cut")]], ",")[[1]])
-                #     r = cut(v, c)
-                #     barplot(table(r))
-                # })
+                output[[paste0("newVars", e, ".tab")]] <- renderTable({
+                    don = donneesAug()
+                    table(don[,input[[paste0("newVars", e, ".nom")]]], useNA = "ifany")
+                })
+                output[[paste0("newVars", e, ".bar")]] <- renderPlot({
+                    don = donneesAug()
+                    barplot(table(don[,input[[paste0("newVars", e, ".nom")]]], useNA = "ifany"))
+                })
             }
         )
         
-        output$newVars.res <- renderTable({
+        donneesAug <- reactive({
             don = donnees()
             
             vars = sub(".var", "", names(input)[grep("newVars[0-9]+.var", names(input))])
-
+            
             if (length(vars) == 0) { return (don)}
             
             for (v in vars) {
@@ -71,7 +75,12 @@ shinyApp(
                 decoupage = as.numeric(strsplit(input[[paste0(v, ".cut")]], ",")[[1]])
                 don[input[[paste0(v, ".nom")]]] = cut(don[[variable]], decoupage)
             }
+            
             don
+        })
+        
+        output$newVars.res <- renderTable({
+            donneesAug()
         })
     }
 )
