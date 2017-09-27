e31c0d453a43e56d018905110f865d94f2519420
fxjollois <fxjolloisupd@gmail.com>
2017-09-27 16:47:40 +0200

    modif global.R -> rappartiement vers server.R

--- global.R
+++ global.R
@@ -4,14 +4,3 @@ library(ggplot2)
 library(scales)
 library(httr)
 
-# Chargement de Recensement85
-recensement85 = read.table("donnees/Recensement85.csv", sep = ",", header = T)
-
-# Fonction permettant d'afficher des pourcentages au lieu de proportions
-to.pct <- function(df, decimal = 0) {
-    nom.lignes = rownames(df)
-    res = sapply(round(df*100, decimal), paste, "%")
-    rownames(res) = nom.lignes
-    return(res)
-}
-# to.pct(as.data.frame.matrix(prop.table(table(mtcars$cyl, mtcars$am))))
--- server.R
+++ server.R
@@ -1,6 +1,17 @@
 # Options pour l'affichage d'un DataTable très basique
 opt.DT.simple = list(paging = FALSE, searching = FALSE, ordering = FALSE, info = FALSE)
 
+# Chargement de Recensement85
+recensement85 = read.table("donnees/Recensement85.csv", sep = ",", header = T)
+
+# Fonction permettant d'afficher des pourcentages au lieu de proportions
+to.pct <- function(df, decimal = 0) {
+    nom.lignes = rownames(df)
+    res = sapply(round(df*100, decimal), paste, "%")
+    rownames(res) = nom.lignes
+    return(res)
+}
+# to.pct(as.data.frame.matrix(prop.table(table(mtcars$cyl, mtcars$am))))
 
 shinyServer(function(input, output, session) {
     
